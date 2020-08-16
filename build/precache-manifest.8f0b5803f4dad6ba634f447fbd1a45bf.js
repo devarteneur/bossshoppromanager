d43a61541bee80e45c86348cdb7c23e87e97f916
George Dimitrakopoulos <gad@inbleck.com>
2020-08-16 17:30:38 +0300

    Removing an Item is now Functional, Added Options per Item

---
+++ build/precache-manifest.8f0b5803f4dad6ba634f447fbd1a45bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f17cf27240566ff786c974611d2747c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aac3cf55e5c5192da6b",
+    "url": "/static/css/2.52de65ad.chunk.css"
+  },
+  {
+    "revision": "cdf2a572eb651f7f91a6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5aac3cf55e5c5192da6b",
+    "url": "/static/js/2.5745a0db.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/static/js/2.5745a0db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf2a572eb651f7f91a6",
+    "url": "/static/js/main.3ec3cabd.chunk.js"
+  },
+  {
+    "revision": "7a54d056cf7b83f8c84f",
+    "url": "/static/js/runtime-main.4c502f40.js"
+  }
+]);
--- src/components/ShopCreator/ShopCreator.js
+++ src/components/ShopCreator/ShopCreator.js
@@ -1,16 +1,7 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Footer, Sidebar, Input, InputGroup, AutoComplete, Row, Col, InputNumber, Button   } from 'rsuite';
+import { Container, Header, Checkbox, Content, Footer, Sidebar, Input, InputGroup, AutoComplete, Row, Col, InputNumber, Button   } from 'rsuite';
 import itemData from '../../mcdata/items'
 import yaml from 'yaml';
-
-const data = [
-    'HYPER Advertiser',
-    'HYPER Web Analytics',
-    'HYPER Video Analytics',
-    'HYPER DMP',
-    'HYPER Ad Serving',
-    'HYPER Data Discovery'
-  ];
 
 const sampleShop = {
     ShopName: "TestShop",
@@ -30,87 +21,107 @@ class ShopCreator extends Component {
     constructor(props){
         super(props)
         this.state = {
+            buyOption:true,
+            sellOption:true,
+            buyBulkOption:true,
+            sellBulkOption: true,
+            sellAllOption:true,
             itemList:[]
         }
     }
+    
+    capitalize(input) {  
+        var CapitalizeWords = input[0].toUpperCase();  
+        for (var i = 1; i <= input.length - 1; i++) {  
+            let currentCharacter,  
+                previousCharacter = input[i - 1];  
+            if (previousCharacter && previousCharacter == ' ') {  
+                currentCharacter = input[i].toUpperCase();  
+            } else {  
+                currentCharacter = input[i];  
+            }  
+            CapitalizeWords = CapitalizeWords + currentCharacter;  
+        }  
+        return CapitalizeWords;  
+    }  
 
-    generateItem(itemName, buyPrice, sellPrice, invLoc){
+    generateItem(itemName, buyPrice, sellPrice, invLoc, options){
+        options = options || {buy:false, sell:true, bulkBuy:false, bulkSell:false, massSell:true}
+        let typeString = `type:${itemName}`; //Dynamic
+        let niceName = this.capitalize(itemName.replace(/_/g, ' '))
+        let displayString = `name:&9${niceName}` //Dynamic
+        let Price = buyPrice;
+        let PriceType_right = sellPrice;
+        console.log(options)
         let thisItem = {
             MenuItem: [
               //'type:DIAMOND',
               'amount:1',
               //'name:&9Diamond',
-              'lore1:&2aristero klik &egia na agoraseis &b%reward% &egia &c%price%&e.',
-              'lore2:&2dexi klik &egia na pouliseis &c%price_right% &egia &b%reward_right%&e.',
-              'lore3:&2shift kai aristero klik &egia na agoraseis &b%reward_shift_left% &egia &c%price_shift_left%&e.',
-              'lore4:&2shift kai dexi klik &egia na pouliseis &c%price_shift_right% &egia &b%reward_shift_right%&e.',
-              'lore5:&2middle klik &egia na pouliseis olo to inventory sou apo to item'
             ],
-            RewardType: 'ITEM',
-            Reward: [  //'type:DIAMOND',
-             'amount:1'  ],
-            PriceType: 'MONEY',
-            //Price: 450,
-            Message: '&eMolis agorases %reward%! Sou emeinan: &c%left%',
-            RewardType_right: 'MONEY',
-            //Reward_right: 112,
-            PriceType_right: 'ITEM',
-            Price_right: [ //'type:DIAMOND', 
-            'amount:1' ],
-            Message_right: '&eMolis poulises %price_right%!',
-            RewardType_shift_left: 'ITEM',
-            Reward_shift_left: [ //'type:DIAMOND', 
-            'amount:16' ],
-            PriceType_shift_left: 'MONEY',
-            //Price_shift_left: 7200,
-            Message_shift_left: '&eMolis agorases %reward_shift_left%! Sou emeinan: &c%left%',
-            RewardType_shift_right: 'MONEY',
-            //Reward_shift_right: 1792,
-            PriceType_shift_right: 'ITEM',
-            Price_shift_right: [ //'type:DIAMOND',
-             'amount:16' ],
-            Message_shift_right: '&eMolis poulises %price_shift_right%!',
-            RewardType_middle: 'MONEY',
-            //Reward_middle: 100,
-            PriceType_middle: 'ITEMALL',
-            Price_middle: [
-                // 'type:DIAMOND' 
-        ],
-            Message_middle: '&eMolis poulises olo to inv gia %price_middle%!',
             ExtraPermission: '',
             //InventoryLocation: 0
           }
 
-        let typeString = `type:${itemName}`; //Dynamic
-        let displayString = `name:&9${itemName}` //Dynamic
-        let Price = buyPrice;
-        let PriceType_right = sellPrice;
-        let Price_shift_left = buyPrice * 16;
-        let Reward_shift_right = sellPrice * 16;
-        let Reward_middle = sellPrice * (1 - 0.10); //10% lower sale price for bulk sales
-
-        //Type
         thisItem.MenuItem.push(typeString)
         thisItem.MenuItem.push(displayString)
-        thisItem.Reward.push(typeString)
-        thisItem.Price_right.push(typeString)
-        thisItem.Reward_shift_left.push(typeString)
-        thisItem.Price_shift_right.push(typeString)
-        thisItem.Price_middle.push(typeString)
 
-        //Pricing
-        thisItem.Price = Price;
-        thisItem.Reward_right = PriceType_right;
-        thisItem.Price_shift_left = Price_shift_left;
-        thisItem.Reward_shift_right = Reward_shift_right;
-        thisItem.Reward_middle = Reward_middle;
+        if(options.buy){
+            thisItem.MenuItem.push('lore1:&2aristero klik &egia na agoraseis &b%reward% &egia &c%price%&e.')
+            thisItem.Price = Price;
+            thisItem.RewardType = 'ITEM';
+            thisItem.Reward = ['amount:1'];
+            thisItem.Reward.push(typeString)
+            thisItem.PriceType = 'Money';
+            thisItem.Message =  '&eMolis agorases %reward%! Sou emeinan: &c%left%';
+        }
+        
+        if(options.sell){
+            thisItem.MenuItem.push('lore2:&2dexi klik &egia na pouliseis &c%price_right% &egia &b%reward_right%&e.')
+            thisItem.Reward_right = PriceType_right;
+            thisItem.RewardType_right = 'MONEY';
+            thisItem.PriceType_right = 'ITEM';
+            thisItem.Price_right = [ 'amount:1' ];
+            thisItem.Price_right.push(typeString)
+            thisItem.Message_right = '&eMolis poulises %price_right%!';
+        }
 
-        //invLoc
+        if(options.bulkBuy){   
+            thisItem.MenuItem.push('lore3:&2shift kai aristero klik &egia na agoraseis &b%reward_shift_left% &egia &c%price_shift_left%&e.')
+            let Price_shift_left = buyPrice * 16;
+            thisItem.Price_shift_left = Price_shift_left;
+            thisItem.RewardType_shift_left = 'ITEM';
+            thisItem.Reward_shift_left = ['amount:16'];
+            thisItem.Reward_shift_left.push(typeString)
+            thisItem.PriceType_shift_left = 'MONEY';
+            thisItem.Message_shift_left = '&eMolis agorases %reward_shift_left%! Sou emeinan: &c%left%';
+        }
+
+        if(options.bulkSell){
+            thisItem.MenuItem.push('lore4:&2shift kai dexi klik &egia na pouliseis &c%price_shift_right% &egia &b%reward_shift_right%&e.')
+            let Reward_shift_right = sellPrice * 16;
+            thisItem.Reward_shift_right = Reward_shift_right;
+            thisItem.RewardType_shift_right = 'MONEY';
+            thisItem.PriceType_shift_right = 'ITEM';
+            thisItem.Price_shift_right = ['amount:16'];
+            thisItem.Price_shift_right.push(typeString)
+            thisItem.Message_shift_right = '&eMolis poulises %price_shift_right%!';
+        }
+
+        if(options.massSell){
+            thisItem.MenuItem.push('lore5:&2middle klik &egia na pouliseis olo to inventory sou apo to item')
+            let Reward_middle = sellPrice * (1 - 0.10); //10% lower sale price for bulk sales
+            thisItem.Reward_middle = Reward_middle;
+            thisItem.RewardType_middle = 'MONEY';
+            thisItem.PriceType_middle = 'ITEMALL';
+            thisItem.Price_middle = [];
+            thisItem.Price_middle.push(typeString)
+            thisItem.Message_middle = '&eMolis poulises olo to inv gia %price_middle%!';
+        }
+
+
         thisItem.InventoryLocation = invLoc;
 
-        // sampleShop.shop[itemName] = thisItem
-
-        // return sampleShop;
         return thisItem;
     }
 
@@ -124,74 +135,88 @@ class ShopCreator extends Component {
     }
 
     handleData(){
-        let iname = this.state.itemName
-        let bprice = this.state.buyPrice
-        let sprice = this.state.sellPrice
+        let iname = this.state.itemName;
+        let bprice = this.state.buyPrice;
+        let sprice = this.state.sellPrice;
+        let options = {buy:this.state.buyOption, sell:this.state.sellOption, bulkBuy:this.state.buyBulkOption, bulkSell:this.state.sellBulkOption, massSell:this.state.sellAllOption};
+
         console.log('adding to table')
         let newItemList = this.state.itemList.concat({
             iname,
             bprice,
-            sprice
+            sprice,
+            options
         })
         this.setState({itemList:newItemList})
-        console.log(`${this.state.itemName} ${this.state.buyPrice} ${this.state.sellPrice} `)
+        console.log(`${this.state.itemName} ${this.state.buyPrice} ${this.state.sellPrice} ${options} `)
     }
 
     renderItemColumns(){
         let content = [];
 
-        return this.state.itemList.map(item=>{
+        return this.state.itemList.map((item, index)=>{
            return(
             <Row key={item.iname} className="show-grid">
-                <Col xs={4}>{item.iname}</Col>
-                <Col xs={4}>{item.bprice}</Col>
-                <Col xs={4}>{item.sprice}</Col>
-                <Col xs={4}><Button>X</Button></Col>
+                <Col xs={2}>{item.iname}</Col>
+                <Col xs={1}>{item.bprice}</Col>
+                <Col xs={1}>{item.sprice}</Col>
+                <Col xs={8}>{JSON.stringify(item.options)}</Col>
+                <Col xs={4}><Button onClick={()=>{this.deleteItemFromShop(item.iname, index)}}>X</Button></Col>
             </Row>
            )
         })   
     }
 
-    
+    deleteItemFromShop(itemName, index){
+        console.log(`Deleting...${itemName}`)
+
+        let iname = this.state.itemName
+        let newItemList =  this.state.itemList
+        newItemList.splice(index , 1)
+        this.setState({itemList:newItemList})
+        console.log(this.state.itemList)
+
+    }
 
     generateShop(){
         let i = 0;
-        sampleShop.ShopName = this.state.shopName
-        sampleShop.DisplayName = this.state.displayName
-        sampleShop.Command = this.state.command
-        sampleShop.signs.text = this.state.signText
-
+        let newShop = {}
+        newShop = sampleShop;
+        newShop.ShopName = this.state.shopName;
+        newShop.DisplayName = this.state.displayName;
+        newShop.Command = this.state.command;
+        newShop.signs.text = this.state.signText;
+        newShop.shop = {}
+        console.log('Items to Generate...')
+        console.log(this.state.itemList)
         for(let item in this.state.itemList){
             
-            sampleShop.shop[this.state.itemList[item].iname] = this.generateItem(this.state.itemList[item].iname, this.state.itemList[item].bprice, this.state.itemList[item].sprice, i)
+            newShop.shop[this.state.itemList[item].iname] = this.generateItem(this.state.itemList[item].iname, this.state.itemList[item].bprice, this.state.itemList[item].sprice, i+1, this.state.itemList[item].options)
             i++;
             console.log(i)
             console.log(this.state.itemList.length)
             if(i >= this.state.itemList.length){
-                navigator.clipboard.writeText(yaml.stringify(sampleShop))
-                console.log(sampleShop)
+                navigator.clipboard.writeText(yaml.stringify(newShop))
+                console.log(newShop)
             }
-        }
-
-   
-        
+        }        
     }
 
     render(){
         console.log(this.state)
         //console.log(yaml.stringify(sampleShop))
         //console.log(yaml.stringify(this.generateItem('DIAMOND', 400, 112, 0)))
-        navigator.clipboard.writeText(yaml.stringify(this.generateItem('DIAMOND', 400, 112, 0)))
+        //navigator.clipboard.writeText(yaml.stringify(this.generateItem('DIAMOND', 400, 112, 0)))
         //console.log(this.returnItemList())
         
         return(
-            <Container>
+            <Container style={{marginTop:'3vw', marginLeft: '5vw', marginRight: '5vw'}}>
                 <Header>
                     <h1>Shop Creator</h1>
                     <h2>Use this tool to create separate shops for your server</h2>
                 </Header>
                 <hr />
-                <Content>
+                <Content >
                 <Row className="show-grid">
                     <Col xs={6}><Input placeholder="Shop Name" onChange={(e)=>{this.setState({shopName:e})}} value={this.state.shopName} /></Col>
                     <Col xs={6}><Input placeholder="Display Name"  onChange={(e)=>{this.setState({displayName:e})}}  /></Col>
@@ -205,6 +230,13 @@ class ShopCreator extends Component {
                     <Col xs={8}>
                         <InputNumber step={10} onChange={(e)=>{this.setState({sellPrice:e})}} placeholder="Sell Price"/>
                     </Col>
+                </Row>
+                <Row className="show-grid">
+                    <Col xs={4}><Checkbox onChange={()=>{this.setState({buyOption:!this.state.buyOption})}} defaultChecked> Buy</Checkbox></Col>
+                    <Col xs={4}><Checkbox onChange={()=>{this.setState({sellOption:!this.state.sellOption})}} defaultChecked> Sell</Checkbox></Col>
+                    <Col xs={4}><Checkbox onChange={()=>{this.setState({buyBulkOption:!this.state.buyBulkOption})}} defaultChecked> 16 Buy</Checkbox></Col>
+                    <Col xs={4}><Checkbox onChange={()=>{this.setState({sellBulkOption:!this.state.sellBulkOption})}} defaultChecked> 16 Sell</Checkbox></Col>
+                    <Col xs={4}><Checkbox onChange={()=>{this.setState({sellAllOption:!this.state.sellAllOption})}} defaultChecked> Sell All</Checkbox></Col>
                 </Row>
                 <br />
                 <Row className="show-grid">
